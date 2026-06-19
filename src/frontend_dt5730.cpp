9171d45fe91fbeeb54d587dd976615b0ab41c400
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2026-06-19 18:44:50 +0900

    Update frontend_dt5730.cpp

--- src/frontend_dt5730.cpp
+++ src/frontend_dt5730.cpp
@@ -31,13 +31,24 @@ int main(int argc, char** argv) {
     int max_events = 0;       
     int run_time_sec = 0;     
 
+    // 🚀 다중 프로세스 듀얼 제어용 식별 변수 추가
+    int link_num = 0;
+    int zmq_port = 5555;
+
     int opt;
-    while ((opt = getopt(argc, argv, "c:o:n:t:")) != -1) {
+    // 🚀 getopt 파서에 l(USB 링크)과 p(ZMQ 포트) 옵션 추가
+    while ((opt = getopt(argc, argv, "c:o:n:t:l:p:h")) != -1) {
         switch (opt) {
             case 'c': config_file = optarg; break;
             case 'o': output_file = optarg; break;
             case 'n': max_events = std::stoi(optarg); break;
             case 't': run_time_sec = std::stoi(optarg); break;
+            case 'l': link_num = std::stoi(optarg); break;
+            case 'p': zmq_port = std::stoi(optarg); break;
+            case 'h':
+            default:
+                std::cout << "Usage: " << argv[0] << " -c <config> [-o <out.dat>] [-n <events>] [-t <sec>] [-l <usb_link>] [-p <zmq_port>]\n";
+                return 1;
         }
     }
 
@@ -48,10 +59,14 @@ int main(int argc, char** argv) {
         PrintConfigContent(config_file);
         auto t = std::time(nullptr);
         auto tm = *std::localtime(&t);
+        
+        // 🚀 기존 시스템 부팅 로깅 완벽 보존 및 링크/포트 정보 병합
         std::cout << "\033[1;32m[Frontend] System Boot Time : \033[0m" << std::put_time(&tm, "%Y-%m-%d %H:%M:%S") << "\n"
-                  << "\033[1;34m[Frontend] Output Target    : \033[0m" << output_file << "\n";
+                  << "\033[1;34m[Frontend] Output Target    : \033[0m" << output_file << "\n"
+                  << "\033[1;35m[Frontend] USB Link / ZMQ   : \033[0mBoard " << link_num << " / Port " << zmq_port << "\n";
 
-        DAQManager daq(config_file, output_file, max_events, run_time_sec);
+        // 🚀 DAQManager에 6개의 인자 모두 전달
+        DAQManager daq(config_file, output_file, max_events, run_time_sec, link_num, zmq_port);
         
         // 메인 루프에 플래그 전달
         daq.Start(g_is_running);
